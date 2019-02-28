45b29e0451f06ddb8f1ff81ae4a3626eca3c9f10
jcm300 <zeclmartins@gmail.com>
2019-02-28 14:47:09 +0000

    fixes on code

--- NumberToText.py
+++ NumberToText.py
@@ -3,7 +3,8 @@
 import re, sys
 
 mapping_units = {
-                    "0":"zero",
+                    "0":"",
+                    "zero":"zero",
                     "1":"um",
                     "2":"dois",
                     "3":"três",
@@ -52,43 +53,66 @@ mapping_hundreds = {
                 }
 
 mapping_ord = {
-                0: " ",
-                1: " mil ",
-                2: " milhões ",
-                3: " mil milhões ",
-                4: " bilhões ",
-                5: " triliões "
+                0: "",
+                1: " mil",
+                2: " milhões",
+                3: " mil milhões",
+                4: " bilhões",
+                5: " triliões"
               }
 
 def toString(text):
+    # String a devolver
     out = ""
+    if text[0]==",": #se começar por uma virgula colocar já no resultado final
+        out += " virgula " 
+    # remover espaços e virgulas
+    text = [t for t in text if t!=" " and t!=","]
+    #tamanho do número recebido
     sizeT = size = len(text)
-    while size>0:
-        rest = size % 3
-        if rest == 1:
-            if sizeT-size-1 < sizeT:
-                if text[sizeT-size-1] != "1":
+    # Se for apenas 0
+    if text=="0":
+        out += mapping_units["zero"];    
+    else:
+        #percorre-se o número do mais signficativo para o menos significativo
+        #contudo por forma a saber a grandeza é necessário que o contador vá do tamanho total do número para 0
+        #por forma a saber a posição de cada número
+        #o resto será a ordem dentro de centenas(0), dezenas(2) e unidades(1)
+        #a divisão inteira dará o número de vezes que há 3 algarismos, ou seja centenas, dezenas, unidades
+        while size>0:
+            rest = size % 3 #calcular a ordem
+            if rest == 1: #ordem das unidades
+                if sizeT-size-1>=0: 
+                    if text[sizeT-size-1] != "1": #se esse valor antes é diferente de 1, de forma a garantir que n entra em conflito com o caso especial das dezenas
+                        out += mapping_units[text[sizeT-size]]
+                        out += mapping_ord[size // 3]
+                else: #senao existe valor antes então é apenas unidade
                     out += mapping_units[text[sizeT-size]]
                     out += mapping_ord[size // 3]
-            else:
-                out += mapping_units[text[sizeT-size]]
-                out += mapping_ord[size // 3]
-                out += " e "
-        elif rest == 2:
-            if text[sizeT-size] == "1":
-                out += mapping_dozens[text[sizeT-size]+text[sizeT-size+1]]
-            else:
-                out += mapping_dozens[text[sizeT-size]]
-                out += " e "
-        else: #rest == 0
-            out += mapping_hundreds[text[sizeT-size]] + " e "
-        size -= 1
+                if sizeT-size+1 < sizeT and text[sizeT-size+1]!="0": #se existe um número|,|espaço a seguir e é diferente de 0
+                    out += " e "
+            elif rest == 2: #ordem das dezenas
+                if text[sizeT-size] == "1": #se o algarismo das dezenas é 1 então a forma como é escrito é especial
+                    out += mapping_dozens[text[sizeT-size]+text[sizeT-size+1]]
+                else: #senão
+                    out += mapping_dozens[text[sizeT-size]]
+                    if text[sizeT-size+1]!="0": #se o algarismo das unidades é diferente de 0
+                        out += " e "
+            else: #rest == 0, ordem das centenas
+                out += mapping_hundreds[text[sizeT-size]]
+                if text[sizeT-size+1]!="0": #se o algarismo das dezenas é diferente de 0
+                    out += " e "
+            size -= 1
     
     return out
 
 def number_to_text(text):
-    text = re.sub(r"[0-9]+",lambda x: toString(x[0]),text)
-    text = re.sub(r",","vírgula ",text)
+    #match com a parte decimal do número
+    text = re.sub(r" ?,([0-9 ]+)?[0-9]",lambda x: toString(x[0]),text)
+    #match com a parte inteira do número que tem de começar e acabar num número podendo ter espaços pelo meio
+    text = re.sub(r"[0-9]([0-9 ]+)?[0-9]",lambda x: toString(x[0]),text)
+    #match com números com apenas unidade, os casos que não foram apanhados pelo caso anterior
+    text = re.sub(r"[0-9]",lambda x: toString(x[0]),text)
     return text
 
 def number_to_text_file(f):
